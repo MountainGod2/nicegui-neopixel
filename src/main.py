34ec37e183a7565010c8b44b9c5b7b0ca57c7c29
MountainGod2 <admin@reid.ca>
2024-01-19 23:28:09 -0700

    Update LED strip control UI

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
-import asyncio
 import board
 import neopixel
 from nicegui import ui
 import webcolors
 
-# Check the available digital pins from the adafruit board module
+# Define the available digital pins from the adafruit board module
 pins = [pin for pin in dir(board) if pin.startswith("D")]
 
 
@@ -47,48 +46,59 @@ class LEDStrip:
         num_leds = int(self.num_leds_input.value)
         brightness = self.brightness_slider.value
         self.update_led_strip(color, pin, num_leds, brightness)
-        await asyncio.sleep(0)
 
 
 @ui.page("/")
 async def index():
-    with ui.card():
-        led_strip_control = LEDStrip(pin="D18", num_leds=30, brightness=0.1)
-        with ui.column():
-            pin_dropdown_label = ui.label(text="Select a pin:")
-            led_strip_control.pin_dropdown = ui.select(
-                value="D18",
-                options=pins,
-                on_change=led_strip_control.handle_update,
-            )
-            ui.separator()
+    # Initialize with a default theme color
+    initial_color = "#ff94b6"
+    ui.colors(primary=initial_color, secondary="#f8b5c0")
+    ui.label.default_style(add=f"color: {initial_color}; font-weight: bold;")
 
-            num_leds_label = ui.label(text="Number of LEDs:")
-            led_strip_control.num_leds_input = ui.number(
-                value=1,
-                min=1,
-                on_change=led_strip_control.handle_update,
-            )
-            ui.separator()
+    led_strip_control = LEDStrip(pin="D18", num_leds=30, brightness=0.1)
+    with ui.element().style(
+        f"background-color: {initial_color}; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);border:3px solid #f8b5c0; border-radius: 15px; "
+    ):
+        with ui.card().style("border-radius: 15px;").classes("no-shadow border-[1px]"):
+            with ui.column():
+                pin_dropdown_label = ui.label(text="Select a pin:")
+                led_strip_control.pin_dropdown = ui.select(
+                    value="D18",
+                    options=pins,
+                    on_change=led_strip_control.handle_update,
+                )
+                ui.separator()
 
-            color_picker_label = ui.label(text="Select a color:")
-            led_strip_control.color_picker = ui.color_input(
-                value="#ff0000",
-                on_change=led_strip_control.handle_update,
-            )
-            ui.separator()
+                num_leds_label = ui.label(text="Number of LEDs:")
+                led_strip_control.num_leds_input = ui.number(
+                    value=1,
+                    min=1,
+                    on_change=led_strip_control.handle_update,
+                )
+                ui.separator()
 
-            brightness_slider_label = ui.label(text="Adjust brightness:")
-            led_strip_control.brightness_slider = ui.slider(
-                value=0.1,
-                min=0.1,
-                max=0.4,  # Limited due to LED brightness, can be increased if needed (up to 1.0)
-                step=0.01,
-                on_change=led_strip_control.handle_update,
-            )
-            ui.separator()
+                color_picker_label = ui.label(text="Select a color:")
+                led_strip_control.color_picker = ui.color_input(
+                    value=initial_color,
+                    on_change=led_strip_control.handle_update,
+                )
+                ui.separator()
 
-            off_button = ui.button(text="Turn Off", on_click=led_strip_control.turn_off)
+                brightness_slider_label = ui.label(text="Adjust brightness:")
+                led_strip_control.brightness_slider = ui.slider(
+                    value=0.1,
+                    min=0.1,
+                    max=0.4,
+                    step=0.01,
+                    on_change=led_strip_control.handle_update,
+                )
+                ui.separator()
+
+                ui.button(
+                    text="Turn Off",
+                    on_click=led_strip_control.turn_off,
+                    color="primary",
+                ).style("align-self: center; border-radius: 15px;")
 
 
 ui.run(title="LED Strip Control")
