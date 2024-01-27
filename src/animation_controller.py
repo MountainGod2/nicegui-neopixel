820bb6776c9b019ad582025977f3a659491eab30
MountainGod2 <admin@reid.ca>
2024-01-27 15:24:19 -0700

    Update naming and remove old files

--- src/animation_controller.py
+++ src/animation_controller.py
@@ -1,261 +1,283 @@
 import asyncio
-
 import board
 import neopixel
 from adafruit_led_animation.animation.comet import Comet
-from adafruit_led_animation.animation.pulse import Pulse
-from adafruit_led_animation.color import BLACK, RED
-from adafruit_led_animation.sequence import AnimationSequence
+from adafruit_led_animation.animation.rainbow import Rainbow
+from adafruit_led_animation.animation.blink import Blink
 from nicegui import app, ui
 
-num_pixels = 100
-pixel_pin = board.D18
+# Constants
+PIXEL_COUNT = 100
+BRIGHTNESS = 0.2
 
+# Initialize NeoPixel strip
+pixels = neopixel.NeoPixel(
+    board.D18, PIXEL_COUNT, brightness=BRIGHTNESS, auto_write=False
+)
 
-class AnimationController:
-    def __init__(self):
-        self.comet_speed = 0.01
-        self.comet_color = RED
-        self.comet_bounce = True
-        self.comet_background_color = BLACK
-        self.comet_tail_length = 10
-        self.pulse_speed = 0.01
-        self.pulse_color = RED
-        self.pulse_period = 2
-        self.running = False
-        self.strip_pixels = None
-
-    def create_pixels(self):
-        self.strip_pixels = neopixel.NeoPixel(
-            pin=pixel_pin, n=num_pixels, brightness=0.1, auto_write=False
-        )
-        return self.strip_pixels
-
-    def create_animations(self, strip_pixels):
-        comet_animation = Comet(
-            pixel_object=strip_pixels,
-            speed=self.comet_speed,
-            color=self.comet_color,
-            bounce=self.comet_bounce,
-            background_color=self.comet_background_color,
-            tail_length=self.comet_tail_length,
-            name="Comet",
-        )
-
-        pulse_animation = Pulse(
-            pixel_object=strip_pixels,
-            speed=self.pulse_speed,
-            color=self.pulse_color,
-            period=self.pulse_period,
-            name="Pulse",
-        )
-
-        self.animation_sequence = AnimationSequence(
-            comet_animation,
-            pulse_animation,
-            advance_interval=None,
-            auto_clear=True,
-        )
-        return self.animation_sequence
-
-    def clear_pixels(self, strip_pixels):
-        strip_pixels.fill((0, 0, 0))
-        strip_pixels.show()
-
-    def start_animations(self):
-        if not self.running:
-            self.running = True
-            asyncio.create_task(self.run_animation_sequence())
-
-    async def stop_animations(self):
-        self.running = False
-        self.clear_pixels(self.strip_pixels)
-
-    async def run_animation_sequence(self):
-        while self.running:
-            self.animation_sequence.animate()
-            await asyncio.sleep(0)
-
-    def current_animation(self):
-        app.storage.general.update(
-            {"current_animation": self.animation_sequence.current_animation.name}
-        )
-        print(self.animation_sequence.current_animation.name)
-
-
-def update_comet_speed(value):
-    # Ensure that the function receives a numeric value for speed
-    animation_controller.comet_speed = float(value)
-    # Re-initialize animations to apply changes
-    animation_controller.create_animations(pixels)
-
-
-def update_comet_tail_length(value):
-    # Ensure that the function receives a numeric value for speed
-    animation_controller.comet_tail_length = int(value)
-    # Re-initialize animations to apply changes
-    animation_controller.create_animations(pixels)
-
-
-def update_comet_bounce(value):
-    animation_controller.comet_bounce = bool(value)
-    # Re-initialize animations to apply changes
-    animation_controller.create_animations(pixels)
-
-
-def update_comet_color(value):
-    # Extract the numeric parts from the string 'rgb(0,0,0)' and convert them to integers
-    rgb_values = value.strip("rgb()").split(",")
-    rgb_tuple = tuple(int(val.strip()) for val in rgb_values)
-
-    # Update the comet color with the new RGB tuple
-    animation_controller.comet_color = rgb_tuple
-    # Re-initialize animations to apply changes
-    animation_controller.create_animations(pixels)
-
-
-def update_comet_background_color(value):
-    # Extract the numeric parts from the string 'rgb(0,0,0)' and convert them to integers
-    rgb_values = value.strip("rgb()").split(",")
-    rgb_tuple = tuple(int(val.strip()) for val in rgb_values)
-
-    # Update the comet color with the new RGB tuple
-    animation_controller.comet_background_color = rgb_tuple
-    # Re-initialize animations to apply changes
-    animation_controller.create_animations(pixels)
-
-
-def update_pulse_speed(value):
-    animation_controller.pulse_speed = value
-    # Re-initialize animations to apply changes
-    animation_controller.create_animations(pixels)
-
-
-def update_pulse_period(value):
-    # Ensure that the function receives a numeric value for speed
-    pulse_period_value = int(value)
-    animation_controller.pulse_period = pulse_period_value
-    # Re-initialize animations to apply changes
-    animation_controller.create_animations(pixels)
-
-
-def update_pulse_color(value):
-    # Extract the numeric parts from the string 'rgb(0,0,0)' and convert them to integers
-    rgb_values = value.strip("rgb()").split(",")
-    rgb_tuple = tuple(int(val.strip()) for val in rgb_values)
-
-    # Update the pulse color with the new RGB tuple
-    animation_controller.pulse_color = rgb_tuple
-    # Re-initialize animations to apply changes
-    animation_controller.create_animations(pixels)
-
-
-def toggle_animation():
-    if animation_controller.running:
-        asyncio.create_task(animation_controller.stop_animations())
-    else:
-        animation_controller.start_animations()
-
-
-def next_animation():
-    animation_controller.animation_sequence.next()
-    animation_controller.current_animation()
-    animation_controller.animation_sequence.animate()
-
-
-def previous_animation():
-    animation_controller.animation_sequence.previous()
-    animation_controller.current_animation()
-    animation_controller.animation_sequence.animate()
-
-
-animation_controller = AnimationController()
-pixels = animation_controller.create_pixels()
-animations = animation_controller.create_animations(pixels)
+# Initialize storage for app parameters
 storage = app.storage.general
 
+# Set default values for animation parameters
+blink_params = storage.setdefault(
+    "blink_params",
+    {
+        "speed": 0.1,
+        "color": "(255,0,153)",
+        "name": "blink",
+    },
+)
 
-# Initialize the UI
-@ui.page("/")
-def index():
-    storage.setdefault("current_animation", "")
-    animation_controller.current_animation()
-    with ui.card().classes("mx-auto").style(add="max-width: 500px; height: 700px;"):
-        # Create a label with a large font size and bold text
-        ui.label("Animation Control Panel").style(
-            add="font-size: 1.5rem; font-weight: bold;"
-        ).classes("mx-auto")
-        with ui.row().classes("mx-auto"):
-            with ui.card().bind_visibility_from(
-                storage, "current_animation", lambda value: value == "Comet"
-            ):
-                with ui.column():
-                    ui.label("Comet Settings").style(
-                        add="font-size: 1rem; font-weight: bold;"
-                    )
-                    ui.label("Speed")
-                    ui.slider(
-                        value=animation_controller.comet_speed,
-                        min=0.001,
-                        max=0.1,
-                        step=0.001,
-                        on_change=lambda event: update_comet_speed(event.value),
-                    )
-                    ui.label("Tail Length")
-                    ui.slider(
-                        value=animation_controller.comet_tail_length,
-                        min=1,
-                        max=num_pixels,
-                        step=1,
-                        on_change=lambda event: update_comet_tail_length(event.value),
-                    )
-                    ui.label("Bounce")
-                    ui.checkbox(
-                        value=animation_controller.comet_bounce,
-                        on_change=lambda event: update_comet_bounce(event.value),
-                    )
-                    ui.label("Color")
-                    ui.color_input(
-                        value=animation_controller.comet_color,
-                        on_change=lambda event: update_comet_color(event.value),
-                    )
+comet_params = storage.setdefault(
+    "comet_params",
+    {
+        "speed": 0.1,
+        "color": "(255,0,153)",
+        "background_color": "(0,0,0)",
+        "tail_length": 10,
+        "bounce": True,
+        "reverse": False,
+        "name": "comet",
+    },
+)
 
-            with ui.card().bind_visibility_from(
-                storage, "current_animation", lambda value: value == "Pulse"
-            ):
-                with ui.column():
-                    ui.label("Pulse Settings").style(
-                        add="font-size: 1rem; font-weight: bold;"
-                    )
-                    ui.label("Speed")
-                    ui.slider(
-                        value=animation_controller.pulse_speed,
-                        min=0.01,
-                        max=0.1,
-                        step=0.01,
-                        on_change=lambda event: update_pulse_speed(event.value),
-                    )
-                    ui.label("Period")
-                    ui.slider(
-                        value=animation_controller.pulse_period,
-                        min=1,
-                        max=10,
-                        step=1,
-                        on_change=lambda event: update_pulse_period(event.value),
-                    )
-                    ui.label("Color")
-                    ui.color_input(
-                        value=animation_controller.pulse_color,
-                        on_change=lambda event: update_pulse_color(event.value),
-                    )
+rainbow_params = storage.setdefault(
+    "rainbow_params",
+    {
+        "speed": 0.1,
+        "period": 10,
+        "step": 1,
+        "precompute_rainbow": True,
+        "name": "rainbow",
+    },
+)
 
-        with ui.row().classes("mx-auto"):
-            ui.button(on_click=previous_animation, icon="arrow_back").classes("mx-auto")
-            ui.button("Start/Stop Animation", on_click=toggle_animation).classes(
-                "mx-auto"
+# Set default values for animation control flags
+storage.setdefault("animation_running", False)
+storage.setdefault("stop_requested", False)
+
+
+async def start_animation(animation_select):
+    selected_animation = animation_select.value
+    storage["stop_requested"] = False
+    if selected_animation == "blink":
+        await blink_animation()
+    elif selected_animation == "rainbow":
+        await rainbow_animation()
+    elif selected_animation == "comet":
+        await comet_animation()
+
+
+async def blink_animation():
+    blink_params = storage.get("blink_params")
+    speed = blink_params["speed"]
+    color = tuple(map(int, blink_params["color"].strip("()").split(",")))
+    name = blink_params["name"]
+
+    blink = Blink(pixels, speed=speed, color=color, name=name)
+    await run_animation(blink)
+
+
+async def comet_animation():
+    comet_params = storage.get("comet_params")
+    speed = comet_params["speed"]
+    color = tuple(map(int, comet_params["color"].strip("()").split(",")))
+    background_color = tuple(
+        map(int, comet_params["background_color"].strip("()").split(","))
+    )
+    tail_length = comet_params["tail_length"]
+    bounce = comet_params["bounce"]
+    reverse = comet_params["reverse"]
+    name = comet_params["name"]
+
+    comet = Comet(
+        pixels,
+        speed=speed,
+        color=color,
+        background_color=background_color,
+        tail_length=tail_length,
+        bounce=bounce,
+        reverse=reverse,
+        name=name,
+    )
+    await run_animation(comet)
+
+
+async def rainbow_animation():
+    rainbow_params = storage.get("rainbow_params")
+    speed = rainbow_params["speed"]
+    period = rainbow_params["period"]
+    step = rainbow_params["step"]
+    precompute_rainbow = rainbow_params["precompute_rainbow"]
+    name = rainbow_params["name"]
+
+    rainbow = Rainbow(
+        pixels,
+        speed=speed,
+        period=period,
+        step=step,
+        name=name,
+        precompute_rainbow=precompute_rainbow,
+    )
+    await run_animation(rainbow)
+
+
+def stop_animation():
+    storage["stop_requested"] = True
+
+
+async def run_animation(animation):
+    if not storage.get("animation_running"):
+        try:
+            storage["animation_running"] = True
+            while not storage["stop_requested"]:
+                animation.animate()
+                await asyncio.sleep(0)
+        except (asyncio.CancelledError, KeyboardInterrupt, RuntimeError):
+            pass
+        finally:
+            pixels.fill((0, 0, 0))
+            pixels.show()
+            storage["animation_running"] = False
+            storage["stop_requested"] = False
+
+
+def apply_standard_style(parent, param, label_text):
+    with parent:
+        label = ui.label(label_text).style("font-size: 0.8rem; color: #666;")
+        return label
+
+
+def apply_slider_with_label(
+    parent, param, min_val, max_val, step_val, label_text, param_name
+):
+    with parent:
+        label = apply_standard_style(parent, param, label_text)
+        slider = ui.slider(min=min_val, max=max_val, step=step_val).bind_value(
+            param, param_name
+        )
+        return label, slider
+
+
+def apply_color_input(parent, param, label_text, param_name):
+    with parent:
+        color_input = (
+            ui.color_input(label=label_text)
+            .bind_value(
+                param,
+                param_name,
+                backward=lambda x: f"rgb{x}",
+                forward=lambda x: x.replace("rgb", ""),
             )
-            ui.button(on_click=next_animation, icon="arrow_forward").classes("mx-auto")
+            .style("margin-bottom: 1.5rem; margin-top: -0.5rem;")
+        )
+        return color_input
 
 
-# Start the NiceGUI event loop
-ui.run(title="NiceGUI Animation Controller")
+def apply_checkbox_with_label(parent, param, label_text, param_name):
+    with parent:
+        label = ui.label(label_text).classes("my-auto").style("color: #666;")
+        checkbox = ui.checkbox().bind_value(param, param_name)
+        return label, checkbox
+
+
+@ui.page(path="/")
+def index():
+    ui.colors(primary="#f48fb1")
+    with ui.card().classes("mx-auto p-4 no-shadow border-[1px]").style(
+        "border: 4px solid #f48fb1; border-radius: 15px; background-color: #fce4ec; margin: 20px auto; width: 80%; max-width: 500px; min-width: 300px"
+    ) as container:
+        ui.label("Animation Controller").classes(
+            "mx-auto text-2xl font-bold text-pink-400 dark:text-pink-400"
+        )
+
+        with ui.row().classes("mx-auto"):
+            ui.label("Select Animation:").classes(
+                "my-auto text-pink-400 dark:text-pink-400"
+            )
+            # Animation type selection
+            animation_select = ui.select(
+                ["blink", "comet", "rainbow"], value="rainbow"
+            ).classes("mx-auto")
+
+        # Blink animation controls
+        blink_controls = (
+            ui.element()
+            .bind_visibility_from(animation_select, "value", value="blink")
+            .classes("w-full")
+        )
+        with blink_controls:
+            apply_slider_with_label(
+                blink_controls, blink_params, 0.01, 5, 0.1, "Speed:", "speed"
+            )
+            apply_color_input(blink_controls, blink_params, "Color:", "color")
+
+        # Comet animation controls
+        comet_controls = (
+            ui.element()
+            .bind_visibility_from(animation_select, "value", value="comet")
+            .classes("w-full")
+        )
+        with comet_controls:
+            apply_slider_with_label(
+                comet_controls, comet_params, 0.01, 0.20, 0.01, "Speed:", "speed"
+            )
+            apply_color_input(comet_controls, comet_params, "Color:", "color")
+            apply_color_input(
+                comet_controls,
+                comet_params,
+                "Background Color:",
+                "background_color",
+            )
+            apply_slider_with_label(
+                comet_controls,
+                comet_params,
+                0,
+                100,
+                1,
+                "Tail Length:",
+                "tail_length",
+            )
+            apply_checkbox_with_label(comet_controls, comet_params, "Bounce", "bounce")
+            apply_checkbox_with_label(
+                comet_controls, comet_params, "Reverse", "reverse"
+            )
+
+        # Rainbow animation controls
+        rainbow_controls = (
+            ui.element()
+            .bind_visibility_from(animation_select, "value", value="rainbow")
+            .classes("w-full")
+        )
+        with rainbow_controls:
+            apply_slider_with_label(
+                rainbow_controls,
+                rainbow_params,
+                0.01,
+                0.20,
+                0.01,
+                "Speed:",
+                "speed",
+            )
+            apply_slider_with_label(
+                rainbow_controls, rainbow_params, 1, 100, 1, "Period:", "period"
+            )
+            apply_slider_with_label(
+                rainbow_controls, rainbow_params, 1, 100, 1, "Step:", "step"
+            )
+            apply_checkbox_with_label(
+                rainbow_controls,
+                rainbow_params,
+                "Precompute Rainbow",
+                "precompute_rainbow",
+            )
+
+        # Start and Stop buttons
+        with ui.row().classes("mx-auto mt-4"):
+            ui.button("Stop", on_click=stop_animation).style("border-radius: 25px")
+            ui.button(
+                "Start", on_click=lambda: start_animation(animation_select)
+            ).style("border-radius: 25px")
+
+
+ui.run()
